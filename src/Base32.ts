b1de4ff4273f5ebf72db6e13bc409d544695e23e
Alessio Frittoli <info@alessiofrittoli.it>
2024-12-02 14:52:59 +0100

    chore: add JSDoc comments

--- src/Base32.ts
+++ src/Base32.ts
@@ -1,4 +1,11 @@
-/** The Base32 Variant. */
+/**
+ * Base32 Variant
+ * 
+ * - `'RFC3548'` - Alias for `'RFC4648'`
+ * - `'RFC4648'` - [Base32 from RFC4648](https://tools.ietf.org/html/rfc4648)
+ * - `'RFC4648-HEX'` - [base32hex from RFC4648](https://tools.ietf.org/html/rfc4648)
+ * - `'Crockford'` - [Crockford's Base32](http://www.crockford.com/wrmg/base32.html)
+ */
 export type Variant = 'RFC3548' | 'RFC4648' | 'RFC4648-HEX' | 'Crockford'
 
 
@@ -58,11 +65,20 @@ class Base32
 	 * @param	variant	The Variant to use.
 	 * @param	options	( Optional ) An object defining encoding options.
 	 * @returns	The encoded `data` to Base32 string.
+	 * 
+	 * ---
+	 * 
+	 * Supported variants:
+	 * 
+	 * - `'RFC3548'` - Alias for `'RFC4648'`
+	 * - `'RFC4648'` - [Base32 from RFC4648](https://tools.ietf.org/html/rfc4648)
+	 * - `'RFC4648-HEX'` - [base32hex from RFC4648](https://tools.ietf.org/html/rfc4648)
+	 * - `'Crockford'` - [Crockford's Base32](http://www.crockford.com/wrmg/base32.html)
 	 */
 	static encode(
 		data		: ArrayBuffer | Int8Array | Uint8Array | Uint8ClampedArray,
 		variant		: Variant,
-		options?	: EncodeOptions
+		options?	: EncodeOptions,
 	)
 	{
 		options ||= {}
